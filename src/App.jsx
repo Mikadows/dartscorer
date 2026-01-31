034562df1684b7fcc81d3f61613c6623105080bb
Mikadows <g.siwiorek.jouvet@gmail.com>
2026-01-31 11:21:58 +0100

    chore(web): init React 19 app (Vite + Prettier)

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Dart Scorer</h1>
+      <p>React 19 starter with Prettier.</p>
+    </div>
+  );
+}
